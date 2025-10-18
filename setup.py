6ee7fcba0232487103ef2e6c8d25cc116f6225d0
Robert Moseley <robby.moseley@gmail.com>
2025-10-18 15:15:02 -0700

    cuda fix

--- setup.py
+++ setup.py
@@ -5,10 +5,7 @@ Supports optional GPU installation with CUDA extensions.
 """
 
 from setuptools import setup, find_packages
-import torch
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 import os
-
 
 # Read requirements
 with open('requirements.txt') as f:
@@ -20,39 +17,47 @@ if os.path.exists('requirements-gpu.txt'):
     with open('requirements-gpu.txt') as f:
         gpu_requires = f.read().splitlines()
 
-# Check if CUDA is available
-cuda_available = torch.cuda.is_available()
-
-# CUDA extension (optional)
+# Check if CUDA is available and build extensions
+# Import torch only when needed to avoid build-time dependency issues
 ext_modules = []
-if cuda_available:
-    ext_modules = [
-        CUDAExtension(
-            name='smartkv_cuda',
-            sources=[
-                'smartkv/csrc/quantized_attention.cu',
-                'smartkv/csrc/bit_packing.cu',
-                'smartkv/csrc/bindings.cpp',
-            ],
-            include_dirs=[
-                'smartkv/csrc',
-            ],
-            extra_compile_args={
-                'cxx': ['-O3', '-std=c++17'],
-                'nvcc': [
-                    '-O3',
-                    '--use_fast_math',
-                    '--expt-relaxed-constexpr',
-                    # Support multiple GPU architectures
-                    '-gencode=arch=compute_70,code=sm_70',  # V100
-                    '-gencode=arch=compute_75,code=sm_75',  # T4, RTX 20xx
-                    '-gencode=arch=compute_80,code=sm_80',  # A100
-                    '-gencode=arch=compute_86,code=sm_86',  # RTX 30xx
-                    '-gencode=arch=compute_89,code=sm_89',  # RTX 40xx
-                ]
-            }
-        )
-    ]
+cmdclass = {}
+
+try:
+    import torch
+    from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+    if torch.cuda.is_available():
+        ext_modules = [
+            CUDAExtension(
+                name='smartkv_cuda',
+                sources=[
+                    'smartkv/csrc/quantized_attention.cu',
+                    'smartkv/csrc/bit_packing.cu',
+                    'smartkv/csrc/bindings.cpp',
+                ],
+                include_dirs=[
+                    'smartkv/csrc',
+                ],
+                extra_compile_args={
+                    'cxx': ['-O3', '-std=c++17'],
+                    'nvcc': [
+                        '-O3',
+                        '--use_fast_math',
+                        '--expt-relaxed-constexpr',
+                        # Support multiple GPU architectures
+                        '-gencode=arch=compute_70,code=sm_70',  # V100
+                        '-gencode=arch=compute_75,code=sm_75',  # T4, RTX 20xx
+                        '-gencode=arch=compute_80,code=sm_80',  # A100
+                        '-gencode=arch=compute_86,code=sm_86',  # RTX 30xx
+                        '-gencode=arch=compute_89,code=sm_89',  # RTX 40xx
+                    ]
+                }
+            )
+        ]
+        cmdclass = {'build_ext': BuildExtension}
+except ImportError:
+    # torch not available during build - skip CUDA extensions
+    pass
 
 setup(
     name='smartkv',
@@ -73,8 +78,8 @@ setup(
             'flake8>=6.1.0',
         ]
     },
-    ext_modules=ext_modules if cuda_available else [],
-    cmdclass={'build_ext': BuildExtension} if cuda_available else {},
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
     python_requires='>=3.8',
     classifiers=[
         'Development Status :: 3 - Alpha',
